9be0bdf74f133241eaec6a762ec5e380bc0c2250
Gregoire Bricot <gbricot@c1r2p10.42perpignan.fr>
2023-06-07 19:28:46 +0200

    first push

---
+++ philo/src/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbricot <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/07 14:51:42 by gbricot           #+#    #+#             */
+/*   Updated: 2023/06/07 15:51:55 by gbricot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <string.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+#endif
